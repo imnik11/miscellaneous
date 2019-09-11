ff8cb825b6528b4d4b40c0fc929e3076ce5618d1
imnik11 <45528152+imnik11@users.noreply.github.com>
2019-09-11 13:36:38 +0530

    Create labques.cpp

---
+++ labques.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+void aka(int in,int wh);
+class{
+  int a[10];
+  friend void aka(int in,int wh);
+};
+class{
+    int i,j;
+    public:
+    
+    
+}
+int main(void){
+    //code here
+    int ch;
+    printf("Enter 1 to push\nEnter 2 to pop\nE")
+    while(1){
+        cin>>ch;
+        switch(ch){
+            
+            case 1: 
+        }
+    }
+    return 0;
+}
